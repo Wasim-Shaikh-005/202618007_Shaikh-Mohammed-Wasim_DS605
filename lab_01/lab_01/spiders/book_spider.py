3bd0e0dd4ecfbfeddd45dd7c836b426d1b98274f
Wasim-Shaikh-005 <shkhwsim@gmail.com>
2026-08-06 18:12:02 +0530

    scraping title and price of books from first 10 pages

--- lab_01/lab_01/spiders/book_spider.py
+++ lab_01/lab_01/spiders/book_spider.py
@@ -1,27 +1,25 @@
 import scrapy
-from pydantic import BaseModel
-from scrapy_spider_metadata import Args
 
 
-class MyParams(BaseModel):
-    pages: int
-
-
-class BookSpider(Args[MyParams], scrapy.Spider):
+class BookSpider(scrapy.Spider):
     name = "bookspider"
-    start_urls = ["http://books.toscrape.com/catalogue"]
+    start_url = "http://books.toscrape.com/catalogue"
 
     async def start(self):
-        for start_url in self.start_urls:
-            for index in range(1, self.args.pages + 1):
-                yield scrapy.Request(f"{start_url}/page-{index}.html")
+        # Crawl the first 10 pages
+        for page in range(1, 11):
+            url = f"{self.start_url}/page-{page}.html"
+            yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
+        # Extract links to individual book pages
         book_links = response.css("article.product_pod h3 a::attr(href)").getall()
+
         for book_link in book_links:
-            yield response.follow(book_link, self.parse_book)
+            yield response.follow(book_link, callback=self.parse_book)
 
     def parse_book(self, response):
+        # Extract book details
         yield {
             "title": response.css("h1::text").get(),
             "price": response.css("p.price_color::text").get(),
